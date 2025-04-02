86c191cb2d188b5f6762a68a8c1ce2d3b459e595
benbest123 <94733217+benbest123@users.noreply.github.com>
2025-04-02 17:33:40 +1300

    removed fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,11 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "@/components/NavBar";
+import styles from "@/styles/App.module.css";
 import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { Geist, Geist_Mono } from "next/font/google";
-import { Container } from "react-bootstrap";
-import styles from "@/styles/App.module.css";
-import NavBar from "@/components/NavBar";
 import NextNProgress from "nextjs-progressbar";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import { Container } from "react-bootstrap";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
