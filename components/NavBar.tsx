3af962b806409c260d61503f20bf499bbae23d74
benbest123 <94733217+benbest123@users.noreply.github.com>
2025-04-02 17:38:38 +1300

    removed empty  navbar props

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,8 +1,6 @@
 import Link from "next/link";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 
-interface NavBarProps {}
-
 const NavBar = () => {
   return (
     <Navbar bg="dark" variant="dark" sticky="top" expand="sm" collapseOnSelect>
